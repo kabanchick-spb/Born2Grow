1c639afb0adc14a51d6f5e18c065630c10148716
Kirill Kalachev <kalachev.kir@ya.ru>
2023-11-06 11:32:32 +0300

    safari v0.5.2

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
-var systemMenuHeight = window.innerHeight - document.documentElement.clientHeight;
-
-if (systemMenuHeight > 0) {
-    var isSystemMenuAtTop = systemMenuHeight > window.innerHeight / 2;
-
-    if (isSystemMenuAtTop) {
-        document.body.style.paddingTop = systemMenuHeight + "px";
-    } else {
-        document.body.style.paddingBottom = systemMenuHeight + "px";
+function hasSystemMenu() {
+    return window.visualViewport.height < window.innerHeight;
+  }
+  
+  function getSystemMenuHeight() {
+    if (hasSystemMenu()) {
+      return window.innerHeight - window.visualViewport.height;
     }
-}
+    return 0;
+  }
+  
+  if (hasSystemMenu()) {
+    const systemMenuHeight = getSystemMenuHeight();
+  }
+
+  function setPaddingForSystemMenu() {
+    var systemMenuHeight = getHeightOfSystemMenu(); // Функция, чтобы получить высоту системного меню
+  
+    var header = document.querySelector('.header');
+    var footer = document.querySelector('.footer');
+  
+    if (header) {
+      header.style.paddingTop = systemMenuHeight + 'px'; // Установка отступа сверху
+    }
+  
+    if (footer) {
+      footer.style.paddingBottom = systemMenuHeight + 'px'; // Установка отступа снизу
+    }
+  }
+  
+  setPaddingForSystemMenu();
